4e2b2ce4a75d260a6dd928d80745dc49b5f35f72
Dim <dmotorny@gmail.com>
2016-12-12 02:31:24 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+var $ = require('jquery');
+
+var str = require('./constant');
+
 var App = function() {
-    console.log('App: init');
+    console.log(str);
+    $('body').html('<h1>hello</h1>');
 }
 
 var app = new App();
