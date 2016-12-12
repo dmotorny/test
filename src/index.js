c4ce74009e1a2505ef4a0fcb4f5c2e2bf1f66686
Dim <dmotorny@gmail.com>
2016-12-12 03:29:12 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 var $ = require('jquery');
-
 var str = require('./constant');
+require('./less')
 
 var App = function() {
     console.log(str);
-    $('body').html('<h1>hello1sdfewrwe</h1>');
+    $('body').html('<h1>hello1sdfewrweasdf</h1>');
 };
 
 var app = new App();
